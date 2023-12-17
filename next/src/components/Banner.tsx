69f657a9f4140d3a214ecf74de9ace53e1ef5e12
DongHun, Kim <donghun.kdh@gmail.com>
2023-12-17 16:07:26 +0900

    :bug: 포스트 페이지 Banner min width 설정

--- next/src/components/Banner.tsx
+++ next/src/components/Banner.tsx
@@ -10,7 +10,7 @@ interface Props {
 
 const Banner = ({ title, category, publishedAt }: Props) => {
   return (
-    <section className="relative flex h-[250px] w-[100vw] justify-center bg-gray-500">
+    <section className="relative flex h-[250px] w-[100vw] min-w-[1024px] justify-center bg-gray-500">
       <div className="flex h-full w-full max-w-screen-lg flex-col px-4 py-10">
         <h3 className="mb-2 font-bold text-white">{category}</h3>
         <h2 className="mb-20 text-3xl font-bold text-white">{title}</h2>
