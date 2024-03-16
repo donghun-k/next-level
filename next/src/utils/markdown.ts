aa91cd690003a24552b72c6fcaf1ec023efed88a
DongHun, Kim <donghun.kdh@gmail.com>
2024-03-16 21:37:52 +0900

    :fire: 불필요한 주석 삭제

--- next/src/utils/markdown.ts
+++ next/src/utils/markdown.ts
@@ -5,13 +5,8 @@ export function convertMarkdownToPlainText(markdownText: string): string {
   if (!markdownText) {
     return "";
   }
-  // Markdown을 HTML로 변환
   const htmlText = marked(markdownText) as string;
-
-  // HTML 엔티티를 일반 텍스트로 변환
   const textWithEntitiesConverted = he.decode(htmlText);
-
-  // HTML 태그와 Markdown 강조 표시를 제거하여 Plain 텍스트로 변환
   const plainText = textWithEntitiesConverted
     .replace(/<[^>]*>/g, "")
     .replace(/(\*\*|__)/g, "");
