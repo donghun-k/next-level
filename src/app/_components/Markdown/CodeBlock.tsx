7512b9fe15233c32686817f0230ce292a760a203
DongHun, Kim <donghun.kdh@gmail.com>
2024-08-16 22:23:12 +0900

    :sparkles: CodeBlock 다크/라이트모드 스타일

--- src/app/_components/Markdown/CodeBlock.tsx
+++ src/app/_components/Markdown/CodeBlock.tsx
@@ -1,13 +1,21 @@
+'use client';
+
+import { useTheme } from 'next-themes';
 import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
-import { materialDark } from 'react-syntax-highlighter/dist/esm/styles/prism';
+import {
+  materialDark,
+  materialLight,
+} from 'react-syntax-highlighter/dist/esm/styles/prism';
 
 const CodeBlock = ({ className, children, ...rest }: any) => {
   const match = /language-(\w+)/.exec(className || '');
+
+  const { theme } = useTheme();
   return match ? (
     <SyntaxHighlighter
       PreTag="div"
       language={match[1]}
-      style={materialDark}
+      style={theme === 'dark' ? materialDark : materialLight}
       {...rest}
     >
       {String(children).replace(/\n$/, '')}
