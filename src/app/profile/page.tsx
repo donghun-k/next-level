2fa128db483097775827a433ea46a8fedcd7e69d
DongHun, Kim <donghun.kdh@gmail.com>
2024-08-16 23:21:02 +0900

    :lipstick: Profile 페이지 아바타 테두리 추가

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -14,7 +14,7 @@ const ProfilePage = async () => {
   return (
     <main>
       <section className="mx-auto flex h-[calc(100vh-80px)] max-w-screen-xl items-center justify-center gap-[100px] pb-[80px]">
-        <Avatar className="size-[360px]">
+        <Avatar className="size-[360px] ring-4 ring-slate-300 dark:ring-slate-800">
           <AvatarImage
             src="https://avatars.githubusercontent.com/u/60064471"
             alt="Donghun Kim"
