c6727a696fb6f950e5b835f3b7740f24d9296182
donghun-k <donghun.kdh@gmail.com>
2024-08-24 14:24:39 +0900

    :wrench: Shiki multiple themes를 사용할 수 있도록 tailwind.config 수정

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -73,6 +73,113 @@ const config = {
         'accordion-down': 'accordion-down 0.2s ease-out',
         'accordion-up': 'accordion-up 0.2s ease-out',
       },
+      typography: {
+        DEFAULT: {
+          css: {
+            'h2, h3, h4': {
+              scrollMarginTop: '5rem',
+            },
+            p: {
+              marginTop: '2rem',
+              marginBottom: '2rem',
+            },
+            '.callout-contents > p': {
+              margin: 0,
+            },
+
+            code: {
+              counterReset: 'line',
+            },
+
+            // Inline code only
+            ':not(pre) > code': {
+              fontWeight: 'inherit',
+              position: 'relative',
+              bottom: 1,
+              margin: '0 3px',
+              color: '#eb5757',
+              backgroundColor: 'rgba(135,131,120,0.15)',
+              fontFamily:
+                '"SFMono-Regular", Menlo, Consolas, "PT Mono", "Liberation Mono", Courier, monospace',
+              borderRadius: 3,
+              padding: '0.2em 0.4em',
+              overflowWrap: 'break-word',
+            },
+
+            'code::before': {
+              content: 'none',
+            },
+            'code::after': {
+              content: 'none',
+            },
+
+            'code[data-line-numbers] > [data-line]::before': {
+              counterIncrement: 'line',
+              content: 'counter(line)',
+
+              /* Other styling */
+              display: 'inline-block',
+              width: '1rem',
+              marginRight: '1.4rem',
+              textAlign: 'right',
+              color: 'lightgrey',
+              fontSize: '0.75rem',
+            },
+
+            'code[data-line-numbers-max-digits="2"] > [data-line]::before': {
+              width: '1rem',
+            },
+
+            'code[data-line-numbers-max-digits="3"] > [data-line]::before': {
+              width: '2rem',
+            },
+
+            pre: {
+              paddingRight: 0,
+              paddingLeft: 0,
+              color: 'var(--shiki-light)',
+              backgroundColor: 'var(--shiki-light-bg)',
+              border: '1px solid #e5e7eb',
+            },
+
+            '.dark pre': {
+              backgroundColor: 'var(--shiki-dark-bg)',
+              color: 'var(--shiki-dark)',
+              border: '1px solid #374151',
+            },
+
+            'pre > code > span': {
+              paddingLeft: '1.1rem',
+              paddingRight: '1rem',
+            },
+
+            'pre code span': {
+              color: 'var(--shiki-light)',
+            },
+            '.dark pre code span': {
+              color: 'var(--shiki-dark)',
+            },
+
+            '[data-highlighted-line]': {
+              backgroundColor: 'rgba(253, 224, 71, 0.2)',
+            },
+
+            '.project img': {
+              marginTop: '0px !important',
+            },
+
+            '.project p,ul,li': {
+              fontSize: 15,
+            },
+
+            u: {
+              textUnderlineOffset: '4px',
+              textDecorationThickness: 1,
+              fontWeight: 600,
+            },
+          },
+        },
+      },
     },
   },
   plugins: [require('tailwindcss-animate'), require('@tailwindcss/typography')],
