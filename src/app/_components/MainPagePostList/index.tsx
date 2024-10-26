9a286409c230c72fc10c3371ca8abb058f42e6ae
donghun-k <donghun.kdh@gmail.com>
2024-10-26 21:36:31 +0900

    :sparkles: 메인 페이지에서 포스트 6개씩 보여주기

--- src/app/_components/MainPagePostList/index.tsx
+++ src/app/_components/MainPagePostList/index.tsx
@@ -1,5 +1,8 @@
 'use client';
 
+import { useState } from 'react';
+
+import { Button } from '@/components/ui/button';
 import type { PostData } from '@/services/post';
 
 import MainPagePostListItem from './MainPagePostListItem';
@@ -9,12 +12,35 @@ interface Props {
 }
 
 const MainPagePostList = ({ postDataList }: Props) => {
+  const [displayedPostDataList, setDisplayedPostDataList] = useState<
+    PostData[]
+  >(postDataList.slice(0, 6));
+
+  const handleLoadMore = () => {
+    if (displayedPostDataList.length === postDataList.length) return;
+    setDisplayedPostDataList((prev) => [
+      ...prev,
+      ...postDataList.slice(prev.length, prev.length + 6),
+    ]);
+  };
   return (
-    <ul className="mt-14 flex flex-wrap gap-x-[20px] gap-y-[30px]">
-      {postDataList.map((postData) => (
-        <MainPagePostListItem key={postData.title} postData={postData} />
-      ))}
-    </ul>
+    <>
+      <ul className="mt-14 flex flex-wrap gap-x-[20px] gap-y-[30px]">
+        {displayedPostDataList.map((postData) => (
+          <MainPagePostListItem key={postData.title} postData={postData} />
+        ))}
+      </ul>
+      {displayedPostDataList.length < postDataList.length && (
+        <div className="flex w-full items-center justify-center pb-[30px] pt-[60px]">
+          <Button
+            onClick={handleLoadMore}
+            className="h-[50px] w-[200px] rounded-full"
+          >
+            Load more
+          </Button>
+        </div>
+      )}
+    </>
   );
 };
 
