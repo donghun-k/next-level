4668f00fbc4b6c807764a313e89cda4507fd55c8
donghun-k <donghun.kdh@gmail.com>
2024-10-27 22:12:16 +0900

    :sparkles: Posts Page 추가

---
+++ src/app/posts/page.tsx
@@ -0,0 +1,9 @@
+const PostsPage = () => {
+  return (
+    <main className="mx-auto h-[calc(100vh-80px)] max-w-screen-xl px-[30px]">
+      PostsPage
+    </main>
+  );
+};
+
+export default PostsPage;
