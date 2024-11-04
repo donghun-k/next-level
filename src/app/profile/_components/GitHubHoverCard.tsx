859b2833a481384aa638fd558d33f7c70e1653c4
donghun-k <donghun.kdh@gmail.com>
2024-11-04 22:40:00 +0900

    :sparkles: GitHubHoverCard 트리거 버튼에 링크 설정
    git push

--- src/app/profile/_components/GitHubHoverCard.tsx
+++ src/app/profile/_components/GitHubHoverCard.tsx
@@ -15,28 +15,28 @@ const PROFILE_PAGE_URL = 'https://github.com/donghun-k';
 
 const GitHubHoverCard = () => {
   return (
-    <HoverCard openDelay={300}>
+    <HoverCard openDelay={0}>
       <HoverCardTrigger asChild>
-        <Button type="button">
-          <IoLogoGithub className="mr-2 size-6" />
-          GitHub
-        </Button>
+        <Link href={PROFILE_PAGE_URL} target="_blank">
+          <Button type="button">
+            <IoLogoGithub className="mr-2 size-6" />
+            GitHub
+          </Button>
+        </Link>
       </HoverCardTrigger>
       <HoverCardContent className="size-fit">
-        <Link href={PROFILE_PAGE_URL}>
-          <div className="flex gap-4">
-            <div className="space-y-1">
-              <p className="text-sm font-bold">Donghun Kim</p>
-              <p className="text-sm text-muted-foreground">donghun-k</p>
-            </div>
-            <Avatar>
-              <AvatarImage src={PROFILE_IMAGE_URL} />
-              <AvatarFallback>
-                <Skeleton className="size-full" />
-              </AvatarFallback>
-            </Avatar>
+        <div className="flex gap-4">
+          <div className="space-y-1">
+            <p className="text-sm font-bold">Donghun Kim</p>
+            <p className="text-sm text-muted-foreground">donghun-k</p>
           </div>
-        </Link>
+          <Avatar>
+            <AvatarImage src={PROFILE_IMAGE_URL} />
+            <AvatarFallback>
+              <Skeleton className="size-full" />
+            </AvatarFallback>
+          </Avatar>
+        </div>
       </HoverCardContent>
     </HoverCard>
   );
