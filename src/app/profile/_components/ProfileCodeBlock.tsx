f552f81bb100d098479fad8c15e69f7decf04045
donghun-k <donghun.kdh@gmail.com>
2024-11-11 02:05:42 +0900

    :recycle: ProfileCodeBlock 컴포넌트로 프로필 내용 분리 및 불필요한 github.ts 서비스 파일 삭제

---
+++ src/app/profile/_components/ProfileCodeBlock.tsx
@@ -0,0 +1,18 @@
+import MDXComponent from '@/app/_components/MDXComponent';
+
+const ProfileCodeBlock = async () => {
+  const response = await fetch(
+    'https://api.github.com/repos/donghun-k/donghun-k/contents/README.md'
+  );
+  const data = await response.json();
+  const profileMarkdown = atob(data.content);
+
+  return (
+    <MDXComponent
+      source={profileMarkdown}
+      className="[&_pre]:border-none [&_pre]:bg-transparent"
+    />
+  );
+};
+
+export default ProfileCodeBlock;
--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,23 +1,12 @@
-import { fetchGitHubFileContent } from '@/services/github';
-
-import MDXComponent from '../_components/MDXComponent';
 import GitHubHoverCard from './_components/GitHubHoverCard';
+import ProfileCodeBlock from './_components/ProfileCodeBlock';
 
-const ProfilePage = async () => {
-  const profileMarkdown = await fetchGitHubFileContent({
-    username: 'donghun-k',
-    repo: 'donghun-k',
-    path: 'README.md',
-  });
-
+const ProfilePage = () => {
   return (
     <main>
       <section className="mx-auto flex h-[calc(100vh-80px)] max-w-screen-xl items-center justify-center gap-[100px] pb-[80px]">
         <div className="flex w-fit max-w-[1000px] flex-col items-center justify-center gap-16 overflow-hidden rounded-xl px-8 py-4">
-          <MDXComponent
-            source={profileMarkdown}
-            className="[&_pre]:border-none [&_pre]:bg-transparent"
-          />
+          <ProfileCodeBlock />
           <div>
             <GitHubHoverCard />
           </div>
