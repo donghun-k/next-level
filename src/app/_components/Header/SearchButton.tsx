b07a5a5e7fceba8cf56767d225d7ed4881900d2a
donghun-k <donghun.kdh@gmail.com>
2024-11-20 22:10:28 +0900

    :sparkles: 헤더 검색 버튼 클릭 시 검색 다이얼로그 오픈

--- src/app/_components/Header/SearchButton.tsx
+++ src/app/_components/Header/SearchButton.tsx
@@ -1,16 +1,44 @@
+'use client';
+
+import { useState } from 'react';
 import { IoSearch } from 'react-icons/io5';
 
 import { Button } from '@/components/ui/button';
+import {
+  CommandDialog,
+  CommandEmpty,
+  CommandGroup,
+  CommandInput,
+  CommandList,
+  CommandSeparator,
+} from '@/components/ui/command';
 
 const SearchButton = () => {
+  const [open, setOpen] = useState(false);
+
+  const handleOpen = () => {
+    setOpen(true);
+  };
   return (
-    <Button
-      variant="outline"
-      size="icon"
-      className="border-none shadow-none focus-visible:ring-0"
-    >
-      <IoSearch className="size-6" />
-    </Button>
+    <>
+      <Button
+        variant="outline"
+        size="icon"
+        className="border-none shadow-none focus-visible:ring-0"
+        onClick={handleOpen}
+      >
+        <IoSearch className="size-6" />
+      </Button>
+      <CommandDialog open={open} onOpenChange={setOpen}>
+        <CommandInput placeholder="Type a command or search..." />
+        <CommandList>
+          <CommandEmpty>No results found.</CommandEmpty>
+          <CommandGroup heading="Post titles" />
+          <CommandSeparator />
+          <CommandGroup heading="Tags" />
+        </CommandList>
+      </CommandDialog>
+    </>
   );
 };
 
