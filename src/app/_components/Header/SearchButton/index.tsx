b578c8b633a66d84500efc80162e4783ffe73753
donghun-k <donghun.kdh@gmail.com>
2024-11-21 22:09:19 +0900

    :sparkles: 검색 다이얼로그 기능 완성

---
+++ src/app/_components/Header/SearchButton/index.tsx
@@ -0,0 +1,95 @@
+'use client';
+
+import { useRouter } from 'next/navigation';
+import { useEffect, useState } from 'react';
+import { IoSearch } from 'react-icons/io5';
+
+import { Button } from '@/components/ui/button';
+import {
+  CommandDialog,
+  CommandEmpty,
+  CommandGroup,
+  CommandInput,
+  CommandItem,
+  CommandList,
+  CommandSeparator,
+  CommandShortcut,
+} from '@/components/ui/command';
+import type { PostData } from '@/types/post';
+
+import { getPostDataListAction } from './_actions/posts';
+import { getTagListAction } from './_actions/tags';
+
+const SearchButton = () => {
+  const [open, setOpen] = useState(false);
+  const [postDataList, setPostDataList] = useState<PostData[]>([]);
+  const [tagList, setTagList] = useState<string[]>([]);
+
+  const router = useRouter();
+
+  const handleOpen = () => {
+    setOpen(true);
+  };
+
+  const handleMoveToPostPage = (id: string) => () => {
+    setOpen(false);
+    router.push(`/post/${id}`);
+  };
+
+  const handleMoveToSearchByTag = (tag: string) => () => {
+    setOpen(false);
+    router.push(`/posts?tag=${tag}`);
+  };
+
+  useEffect(() => {
+    (async () => {
+      setPostDataList(await getPostDataListAction());
+      setTagList(await getTagListAction());
+    })();
+  }, []);
+  return (
+    <>
+      <Button
+        variant="outline"
+        size="icon"
+        className="border-none shadow-none focus-visible:ring-0"
+        onClick={handleOpen}
+      >
+        <IoSearch className="size-6" />
+      </Button>
+      <CommandDialog open={open} onOpenChange={setOpen}>
+        <CommandInput placeholder="Type a command or search..." />
+        <CommandList>
+          <CommandEmpty>No results found.</CommandEmpty>
+          <CommandGroup heading="Posts">
+            {postDataList.map((post) => (
+              <CommandItem
+                className="cursor-pointer"
+                key={post.id}
+                onSelect={handleMoveToPostPage(post.id)}
+              >
+                <p>{post.title}</p>
+                <CommandShortcut>Move to this post</CommandShortcut>
+              </CommandItem>
+            ))}
+          </CommandGroup>
+          <CommandSeparator />
+          <CommandGroup heading="Tags">
+            {tagList.map((tag) => (
+              <CommandItem
+                className="cursor-pointer"
+                key={tag}
+                onSelect={handleMoveToSearchByTag(tag)}
+              >
+                <p>{tag}</p>
+                <CommandShortcut>Search by this tag</CommandShortcut>
+              </CommandItem>
+            ))}
+          </CommandGroup>
+        </CommandList>
+      </CommandDialog>
+    </>
+  );
+};
+
+export default SearchButton;
