f3abda7007f561f40c768418d345c62365159973
donghun-k <donghun.kdh@gmail.com>
2024-11-23 02:02:27 +0900

    :bug: Shadcn Dialog와 Sonner 충돌 문제 해결

--- src/components/ui/dialog.tsx
+++ src/components/ui/dialog.tsx
@@ -42,6 +42,14 @@ const DialogContent = React.forwardRef<
         className
       )}
       {...props}
+      onPointerDownOutside={(e) => {
+        if (
+          e.target instanceof Element &&
+          e.target.closest('[data-sonner-toast]')
+        ) {
+          e.preventDefault();
+        }
+      }}
     >
       {children}
       <DialogPrimitive.Close className="absolute right-4 top-4 rounded-sm opacity-70 ring-offset-background transition-opacity hover:opacity-100 focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 disabled:pointer-events-none data-[state=open]:bg-accent data-[state=open]:text-muted-foreground">
--- src/components/ui/sonner.tsx
+++ src/components/ui/sonner.tsx
@@ -11,7 +11,7 @@ const Toaster = ({ ...props }: ToasterProps) => {
   return (
     <Sonner
       theme={theme as ToasterProps['theme']}
-      className="toaster group"
+      className="toaster group pointer-events-auto"
       toastOptions={{
         classNames: {
           toast:
