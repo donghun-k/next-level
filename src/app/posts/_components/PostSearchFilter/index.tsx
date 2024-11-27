caa80c379270bee6a952042bf25d4212389b3986
donghun-k <donghun.kdh@gmail.com>
2024-11-27 21:50:28 +0900

    :sparkles: PostSearchFilter 컴포넌트에 useSearchQuery 훅 연동

--- src/app/posts/_components/PostSearchFilter/index.tsx
+++ src/app/posts/_components/PostSearchFilter/index.tsx
@@ -1,15 +1,28 @@
+'use client';
+
 import { Search } from 'lucide-react';
+import type { ChangeEventHandler } from 'react';
 
 import { Input } from '@/components/ui/input';
 
+import { useSearchQuery } from '../../_hooks/useSearchQuery';
+
 const PostSearchFilter = () => {
+  const { searchQuery, setSearchQuery } = useSearchQuery();
+
+  const handleSearchChange: ChangeEventHandler<HTMLInputElement> = (e) => {
+    setSearchQuery(e.target.value);
+  };
+
   return (
     <section className="flex items-center justify-center p-8">
       <div className="relative h-12 w-full max-w-xl">
         <Search className="absolute left-4 top-1/2 -translate-y-1/2" />
         <Input
+          value={searchQuery}
           className="size-full pl-12 text-xl"
           placeholder="Search posts by title"
+          onChange={handleSearchChange}
         />
       </div>
     </section>
