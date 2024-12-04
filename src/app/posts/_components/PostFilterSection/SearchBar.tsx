4a2553a794bb383087b931289409f24bcd9fdafb
donghun-k <donghun.kdh@gmail.com>
2024-12-04 22:29:49 +0900

    :sparkles: 검색 디바운스 처리 추가

--- src/app/posts/_components/PostFilterSection/SearchBar.tsx
+++ src/app/posts/_components/PostFilterSection/SearchBar.tsx
@@ -1,18 +1,28 @@
 'use client';
 
 import { Search } from 'lucide-react';
-import type { ChangeEventHandler } from 'react';
+import { type ChangeEventHandler, useRef } from 'react';
 
 import { Input } from '@/components/ui/input';
 
 import { useSearchQuery } from '../../_hooks/useSearchQuery';
 
 const SearchBar = () => {
-  const { searchQuery, setSearchQuery } = useSearchQuery();
+  const { setSearchQuery, setIsTyping } = useSearchQuery();
+
+  const timerRef = useRef<NodeJS.Timeout | null>(null);
 
   const handleSearchChange: ChangeEventHandler<HTMLInputElement> = (e) => {
-    setSearchQuery(e.target.value);
+    if (timerRef.current) {
+      clearTimeout(timerRef.current);
+    }
+    setIsTyping(true);
+    timerRef.current = setTimeout(() => {
+      setIsTyping(false);
+      setSearchQuery(e.target.value);
+    }, 1000);
   };
+
   return (
     <div className="relative h-12 w-[400px]">
       <Search
@@ -20,7 +30,6 @@ const SearchBar = () => {
         className="absolute left-3 top-1/2 -translate-y-1/2 text-muted-foreground"
       />
       <Input
-        value={searchQuery}
         className="size-full pl-10"
         placeholder="Search posts by title"
         onChange={handleSearchChange}
