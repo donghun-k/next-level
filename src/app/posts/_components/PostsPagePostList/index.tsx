d26008bbe7de4be98d15819e9a886410e90c97c4
donghun-k <donghun.kdh@gmail.com>
2024-12-05 22:29:36 +0900

    :sparkles: 포스트 목록 페이지네이션 추가

--- src/app/posts/_components/PostsPagePostList/index.tsx
+++ src/app/posts/_components/PostsPagePostList/index.tsx
@@ -5,6 +5,7 @@
 import { useEffect, useState } from 'react';
 
 import { searchPostsAction } from '@/actions/search';
+import PaginationBar from '@/components/common/PaginationBar';
 import type { PostData } from '@/types/post';
 
 import { useSearchQuery } from '../../_hooks/useSearchQuery';
@@ -12,37 +13,52 @@ import { useTagFilter } from '../../_hooks/useTagFilter';
 import PostsPagePostListItem from './PostsPagePostListItem';
 import PostsPagePostListItemSkeleton from './PostsPagePostListItemSkeleton';
 
+const ITEMS_PER_PAGE = 5;
+
 const PostsPagePostList = () => {
   const { tagFilter } = useTagFilter();
   const { searchQuery, isTyping } = useSearchQuery();
 
+  const [page, setPage] = useState(1);
+  const [lastPage, setLastPage] = useState(1);
   const [postDataList, setPostDataList] = useState<PostData[]>([]);
   const [resultCount, setResultCount] = useState(0);
 
   useEffect(() => {
     (async () => {
-      const { posts, totalItems } = await searchPostsAction({
+      const { posts, totalItems, totalPages } = await searchPostsAction({
         tag: tagFilter || undefined,
         query: searchQuery,
+        page,
+        pageSize: ITEMS_PER_PAGE,
       });
       setResultCount(totalItems);
+      setLastPage(totalPages);
       setPostDataList(posts);
     })();
-  }, [tagFilter, searchQuery]);
+  }, [tagFilter, searchQuery, page]);
 
   return (
-    <ul className="mt-4 flex flex-col gap-6">
-      <p className="text-lg font-semibold">
-        {resultCount} result{resultCount !== 1 ? 's' : ''} found
+    <>
+      <p className="mt-4 text-xl">
+        {resultCount} result{resultCount !== 1 ? 's' : ''}
       </p>
-      {isTyping
-        ? Array.from({ length: 5 }).map((_, index) => (
-            <PostsPagePostListItemSkeleton key={index} />
-          ))
-        : postDataList.map((postData) => (
-            <PostsPagePostListItem key={postData.title} postData={postData} />
-          ))}
-    </ul>
+      <ul className="mt-4 flex flex-col gap-6">
+        {isTyping
+          ? Array.from({ length: 5 }).map((_, index) => (
+              <PostsPagePostListItemSkeleton key={index} />
+            ))
+          : postDataList.map((postData) => (
+              <PostsPagePostListItem key={postData.title} postData={postData} />
+            ))}
+      </ul>
+      <PaginationBar
+        lastPage={lastPage}
+        currentPage={page}
+        onChange={setPage}
+        className="mt-8"
+      />
+    </>
   );
 };
 
