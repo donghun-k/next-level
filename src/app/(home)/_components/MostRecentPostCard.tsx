aa29a167979b4f9d28c1d58295ef496aafd41602
donghun-k <donghun.kdh@gmail.com>
2024-12-22 22:49:34 +0900

    :lipstick: MostRecentPostCard 반응형 디자인

--- src/app/(home)/_components/MostRecentPostCard.tsx
+++ src/app/(home)/_components/MostRecentPostCard.tsx
@@ -13,7 +13,7 @@ interface Props {
 
 const MostRecentPostCard = ({ postData }: Props) => {
   return (
-    <article className="flex flex-row justify-between rounded-md bg-card p-8 shadow-sm">
+    <article className="flex flex-row justify-between rounded-md bg-card p-8 shadow-sm max-md:flex-col">
       <Link href={`/post/${postData.id}`}>
         <NextImage
           src={postData.thumbnail}
@@ -23,11 +23,13 @@ const MostRecentPostCard = ({ postData }: Props) => {
           height={600}
         />
       </Link>
-      <div className="flex w-[520px] flex-col justify-between pl-[30px]">
+      <div className="flex w-[520px] flex-col justify-between pl-[30px] max-md:mt-4 max-md:w-full max-md:pl-0">
         <div>
-          <p className="text-sm text-muted-foreground">{postData.date}</p>
+          <p className="text-sm text-muted-foreground max-md:text-xs">
+            {postData.date}
+          </p>
           <Link href={`/post/${postData.id}`}>
-            <h2 className="mt-1 line-clamp-2 break-keep text-3xl font-bold leading-[3rem] hover:underline">
+            <h2 className="mt-1 line-clamp-2 break-keep text-3xl font-bold leading-[3rem] hover:underline max-md:text-2xl">
               {postData.title}
             </h2>
           </Link>
@@ -37,11 +39,13 @@ const MostRecentPostCard = ({ postData }: Props) => {
             ))}
           </div>
         </div>
-        <p className="line-clamp-6 leading-8">
+        <p className="line-clamp-6 leading-8 max-md:my-4 max-md:line-clamp-3 max-md:leading-6">
           {stripMarkdown(postData.content)}
         </p>
-        <Link href={`/post/${postData.id}`} className="size-fit">
-          <Button className="size-fit px-6 py-4">Continue reading</Button>
+        <Link href={`/post/${postData.id}`} className="size-fit max-md:w-full">
+          <Button className="size-fit px-6 py-4 max-md:w-full">
+            Continue reading
+          </Button>
         </Link>
       </div>
     </article>
